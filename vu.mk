17654f9daa29974a20ff5958caedc7048385e439
puk3n <slax775@gmail.com>
2016-04-26 15:03:47 -0400

    **make it vu

---
+++ vu.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/vu/config/nfc_enhanced.mk)
+
+# Inherit some common VU stuff.
+$(call inherit-product, vendor/vu/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltexx \
+  TARGET_DEVICE=jfltexx \
+  BUILD_FINGERPRINT="samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHOB7:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHOB7 release-keys"
+
+PRODUCT_DEVICE := jfltexx
+PRODUCT_NAME := vu_jfltexx
+
